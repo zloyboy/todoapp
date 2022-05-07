a80c4a372fb429d72d7c9096cfa707c4cba0cb11
Vlad Boyko <swampman@inbox.ru>
2022-05-07 13:10:06 +0300

    [add] handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	todo "github.com/zloyboy/todoapp"
+	"github.com/zloyboy/todoapp/internal/handler"
 )
 
 func main() {
+	handler := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatal("run error")
 	}
 }
